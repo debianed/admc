326203a956061cab1e6afe10a45b453368fb67e3
Semyon Knyazev <samael@altlinux.org>
2026-03-23 21:47:40 +0400

    tests: fix segfault in ADMCTest::select_base_widget_add on Qt6
    QTest::qWaitForWindowExposed() crashes in Qt6 if the widget's
    window handle is not yet initialized. Add QApplication::processEvents()
    after browse_button->click() to ensure the dialog is fully constructed
    before passing it to qWaitForWindowExposed().

--- tests/admc_test.cpp
+++ tests/admc_test.cpp
@@ -298,6 +298,7 @@ void ADMCTest::test_edit_apply_unmodified(AttributeEdit *edit, const QString &dn
 void ADMCTest::select_base_widget_add(SelectBaseWidget *widget, const QString &dn) {
     QPushButton *browse_button = widget->ui->browse_button;
     browse_button->click();
+    QApplication::processEvents();
 
     auto select_container_dialog = widget->findChild<SelectContainerDialog *>();
     QVERIFY(select_container_dialog);
